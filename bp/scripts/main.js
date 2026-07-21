b9a71184ae3a7cac85bd7f56c61a7f6e9a11a2bd
MEGAqx123 <MEGAqx123@gmail.com>
2026-07-21 16:01:37 +0800

    pack icon

---
+++ bp/scripts/main.js
@@ -0,0 +1,23 @@
+
+import { world } from "@minecraft/server";
+
+function buy(player, detectItem, detectData, item, price) {
+    try {
+        const result = player.runCommand(
+            `scoreboard players remove @s[scores={Coins=${price}..},hasitem={item=${detectItem},data=${detectData}}] Coins ${price}`
+        );
+
+        if (result.successCount > 0) {
+            player.runCommand(`give @s ${item}`);
+            player.runCommand(`clear @s ${detectItem} 1 ${detectData}`);
+        } else {
+            player.sendMessage("§cNot enough Coins!");
+        }
+    } catch (e) {
+        console.warn(e);
+    }
+}
+
+for (const player of world.getPlayers()) {
+    buy(player, "spyglass", 10, "totem_of_undying", 600);
+}
